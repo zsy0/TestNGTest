1f7b574cfedf0843a2fe5048c12d83cbbbebb70b
ZSY\77854 <syzhsy@qq.com>
2019-03-26 22:30:49 +0800

    no message

--- src/testEx/ComplexOpTest.java
+++ src/testEx/ComplexOpTest.java
@@ -1,30 +1,30 @@
 package testEx;
 
 import org.testng.Assert;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import code.ComplexOp;
 
 public class ComplexOpTest {
 
-  @BeforeTest
-  public ComplexOp beforeTest() {
-	  return new ComplexOp();
-  }
-  
-  @Test
-  public void testMultiply() {
-	  ComplexOp so = beforeTest();
-	  double result = so.multiply(3, 4);
-	  Assert.assertEquals(result,12.0);
-  }
-  
-  @Test
-  public void testDevide() {
-	  ComplexOp so = beforeTest();
-	  double result = so.divide(12,6);
-	  Assert.assertEquals(result,2.0);
-  }
+	@BeforeMethod
+	public ComplexOp beforeTest() {
+		return new ComplexOp();
+	}
+
+	@Test
+	public void testMultiply() {
+		ComplexOp so = beforeTest();
+		double result = so.multiply(3, 4);
+		Assert.assertEquals(result, 12.0);
+	}
+
+	@Test
+	public void testDevide() {
+		ComplexOp so = beforeTest();
+		double result = so.divide(12, 6);
+		Assert.assertEquals(result, 2.0);
+	}
 
 }
